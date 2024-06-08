8f38eeaef21f14334f1a363aa364f6e813eb2332
João Pedro Flausino de Lima <joaoxpp2001@hotmail.com>
2024-06-08 20:20:49 -0300

    navbar ajustavel

--- src/Components/NavBar.tsx
+++ src/Components/NavBar.tsx
@@ -1,13 +1,16 @@
 import React from 'react';
-import { Drawer, List, ListItem, ListItemIcon, ListItemText, Toolbar,Divider, ListItemButton } from '@mui/material';
+import { Drawer, List, ListItem, ListItemIcon, ListItemText, Toolbar, Divider, ListItemButton, IconButton } from '@mui/material';
 import HomeIcon from '@mui/icons-material/Home';
 import NewspaperIcon from '@mui/icons-material/Newspaper';
 import CalculateIcon from '@mui/icons-material/Calculate';
 import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 import AccountBalanceWalletIcon from '@mui/icons-material/AccountBalanceWallet';
 import LogoutIcon from '@mui/icons-material/Logout';
+import MenuIcon from '@mui/icons-material/Menu';
 import { styled } from '@mui/system';
 import { Link } from 'react-router-dom';
+import { useState } from 'react';
+import useMediaQuery from '@mui/material/useMediaQuery';
 
 const drawerWidth = 240;
 
@@ -26,7 +29,7 @@ const StyledDrawer = {
     flexDirection: 'column',
     justifyContent: 'space-between',
   },
-}
+};
 
 const StyledListItemButton = {
   '&:hover': {
@@ -44,16 +47,18 @@ const StyledListItemButton = {
       color: '#000000',
     },
   },
-}
-
+};
 
 const Navbar: React.FC = () => {
-  return (
-    <Drawer
-      variant="permanent"
-      anchor="left"
-      sx={StyledDrawer}
-    >
+  const isSmallScreen = useMediaQuery('(max-width:600px)');
+  const [mobileOpen, setMobileOpen] = useState(false);
+
+  const handleDrawerToggle = () => {
+    setMobileOpen(!mobileOpen);
+  };
+
+  const drawer = (
+    <>
       <Toolbar />
       <List>
         {[
@@ -62,30 +67,62 @@ const Navbar: React.FC = () => {
           { text: 'User Investments', icon: <AccountBalanceWalletIcon />, link: '/addinvestments' },
           { text: 'Investments', icon: <AccountBalanceWalletIcon />, link: '/investments' },
           { text: 'Simulate', icon: <CalculateIcon />, link: '/simulate' },
-          
-        ].map((item, index) => (
-            <ListItemButton sx={StyledListItemButton} key={item.text} component={Link} to={item.link}>
+        ].map((item) => (
+          <ListItemButton sx={StyledListItemButton} key={item.text} component={Link} to={item.link}>
             <ListItemIcon sx={{ color: '#fff' }}>{item.icon}</ListItemIcon>
             <ListItemText primary={item.text} />
           </ListItemButton>
         ))}
       </List>
-      
       <Divider sx={{ backgroundColor: '#fff' }}/>
       <List>
         {[
-            { text: 'Account', icon: <AccountCircleIcon />, link: '/account' },
-            { text: 'Logout', icon: <LogoutIcon />, link: '/' },
-        ].map((item, index) => (
-            <ListItemButton sx={StyledListItemButton} key={item.text} component={Link} to={item.link}>
+          { text: 'Account', icon: <AccountCircleIcon />, link: '/account' },
+          { text: 'Logout', icon: <LogoutIcon />, link: '/' },
+        ].map((item) => (
+          <ListItemButton sx={StyledListItemButton} key={item.text} component={Link} to={item.link}>
             <ListItemIcon sx={{ color: '#fff' }}>{item.icon}</ListItemIcon>
             <ListItemText primary={item.text} />
           </ListItemButton>
         ))}
       </List>
+    </>
+  );
 
-
-    </Drawer>
+  return (
+    <>
+      {isSmallScreen ? (
+        <>
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            edge="start"
+            onClick={handleDrawerToggle}
+            sx={{ ml: 2 }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Drawer
+            variant="temporary"
+            open={mobileOpen}
+            onClose={handleDrawerToggle}
+            ModalProps={{
+              keepMounted: true, // Better open performance on mobile.
+            }}
+            sx={StyledDrawer}
+          >
+            {drawer}
+          </Drawer>
+        </>
+      ) : (
+        <Drawer
+          variant="permanent"
+          sx={StyledDrawer}
+        >
+          {drawer}
+        </Drawer>
+      )}
+    </>
   );
 };
 
